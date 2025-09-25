666ce6c0ca2aabf99c8b73a8dfd21e05f4f9591f
IPham23 <iancoladilla23@gmail.com>
2025-09-25 13:22:02 +0800

    Finished Mainboard page
    done styling, layouting, creating effects and transitions on mainboard page. also added some changes in homepage

--- src/pages/Mainboard.tsx
+++ src/pages/Mainboard.tsx
@@ -5,23 +5,62 @@ const Mainboard = () => {
         <>
         <div className={styles.wrapper}>
             <section className={styles.top}>
+                <button className={styles.kidsBtn}>KIDS</button>
                 <div className={styles.kidsGrid}>
-                    <img className={styles.grayscale} src="./src/assets/K1.1.png" alt="Kid 1" />
-                    <img className={styles.grayscale} src="./src/assets/K2.1.png" alt="Kid 2" />
-                    <img className={styles.grayscale} src="./src/assets/K3.1.png" alt="Kid 3" />
-                    <img className={styles.grayscale} src="./src/assets/K4.1.png" alt="Kid 4" />
+                    <div className={styles.kidsTextLeft}>
+                        <img src="./src/assets/kidsText.png" alt="" />
+                        <img src="./src/assets/kidsText.png" alt="" />
+                        <img src="./src/assets/kidsText.png" alt="" />
+                        <img src="./src/assets/kidsText.png" alt="" />
+                        <img src="./src/assets/kidsText.png" alt="" />
+                        <img src="./src/assets/kidsText.png" alt="" />
+                    </div>
+
+                    <div className={styles.photoWrapper}><img className={styles.kidsPhotos} src="./src/assets/K1.1.png" alt="Kid 1" /></div>
+                    <div className={styles.photoWrapper}><img className={styles.kidsPhotos} src="./src/assets/K2.1.png" alt="Kid 2" /></div>
+                    <div className={styles.photoWrapper}><img className={styles.kidsPhotos} src="./src/assets/K3.1.png" alt="Kid 3" /></div>
+                    <div className={styles.photoWrapper}><img className={styles.kidsPhotos} src="./src/assets/K4.1.png" alt="Kid 4" /></div>
+
+                    <div className={styles.kidsTextRight}>
+                        <img src="./src/assets/kidsText.png" alt="" />
+                        <img src="./src/assets/kidsText.png" alt="" />
+                        <img src="./src/assets/kidsText.png" alt="" />
+                        <img src="./src/assets/kidsText.png" alt="" />
+                        <img src="./src/assets/kidsText.png" alt="" />
+                        <img src="./src/assets/kidsText.png" alt="" />
+                    </div>
                 </div>
+
             </section>
             <section className={styles.center}>
                 <h1>MANILA RUNWAY REPUBLIC MODELS</h1>
             </section>
 
             <section className={styles.bottom}>
+                 <button className={styles.teensBtn}>TEENS</button>
                 <div className={styles.teensGrid}>
-                    <img className={styles.grayscale} src="./src/assets/T1.1.png" alt="Teen 1" />
-                    <img className={styles.grayscale} src="./src/assets/T2.1.png" alt="Teen 2" />
-                    <img className={styles.grayscale} src="./src/assets/T3.1.png" alt="Teen 3" />
-                    <img className={styles.grayscale} src="./src/assets/T4.1.png" alt="Teen 4" />
+                    <div className={styles.teensTextLeft}>
+                        <img src="./src/assets/teensText.png" alt="" />
+                        <img src="./src/assets/teensText.png" alt="" />
+                        <img src="./src/assets/teensText.png" alt="" />
+                        <img src="./src/assets/teensText.png" alt="" />
+                        <img src="./src/assets/teensText.png" alt="" />
+                        <img src="./src/assets/teensText.png" alt="" />
+                    </div>
+
+                    <div className={styles.photoWrapper}><img className={styles.teensPhotos} src="./src/assets/T1.1.png" alt="Teen 1" /></div>
+                    <div className={styles.photoWrapper}><img className={styles.teensPhotos} src="./src/assets/T2.1.png" alt="Teen 2" /></div>
+                    <div className={styles.photoWrapper}><img className={styles.teensPhotos} src="./src/assets/T3.1.png" alt="Teen 3" /></div>
+                    <div className={styles.photoWrapper}><img className={styles.teensPhotos} src="./src/assets/T4.1.png" alt="Teen 4" /></div>
+                    
+                    <div className={styles.teensTextRight}>
+                        <img src="./src/assets/teensText.png" alt="" />
+                        <img src="./src/assets/teensText.png" alt="" />
+                        <img src="./src/assets/teensText.png" alt="" />
+                        <img src="./src/assets/teensText.png" alt="" />
+                        <img src="./src/assets/teensText.png" alt="" />
+                        <img src="./src/assets/teensText.png" alt="" />
+                    </div>
                 </div>
             </section>
         </div>
